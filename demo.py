5b8a57295e804bb0b8c2789980e8760b00ab8ce1
g3n4ik <80119318+g3n4ik@users.noreply.github.com>
2021-12-20 16:39:40 +0500

    Add files via upload

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 import sublime, sublime_plugin
 from urllib import request
+import os
 import urllib
 
 def f(x):
@@ -8,15 +9,22 @@ def f(x):
     for i in range(len(x)):
         ans = ans + "<div> <a href = '" + x[i] + "'" + ">" + x[i] + "</a> </div>"
     return ans
-
-
+'''
+class NameCommand(sublime_plugin.WindowCommand):
+    def run(self, edit):
+        return window.view.file_name()'''
 class ExampleCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         context = self.view.substr(sublime.Region(0, self.view.size()))
-        reg = request.Request("http://localhost:5342/", data=("context=" + context).encode())
+        if (len(context) == 0):
+            return
+        adres = self.view.file_name()
+        data = {'context' : context, 'adres' : adres}
+        data = parse.urlencode(data).encode()
+        reg = request.Request("http://localhost:5342/", data=data)
         page = urllib.request.urlopen(reg)
         content = f(page.read().decode())
-        print(content)
+        content = f(context)
         self.view.show_popup(content, flags=sublime.HTML, location=-1, max_width=400, on_navigate=self.on_choice_symbol)
 
     def on_choice_symbol(self, symbol):
