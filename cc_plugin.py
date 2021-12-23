3f6b5add0845322e6e25e9e9f7511fa72fa62023
topshik <semenkin.anton@gmail.com>
2021-12-23 10:03:02 +0300

    upd: plugin file

--- cc_plugin.py
+++ cc_plugin.py
@@ -5,34 +5,23 @@ from urllib import request, parse
 import sublime, sublime_plugin
 
 
-def render_completions(x):
-    ans = ""
-    x = x.split("\n")
-    for i in range(len(x)):
-        ans = ans + "<div> <a href = '" + x[i] + "'" + ">" + x[i] + "</a> </div>"
-        
-    return ans
-
-
 class ExampleCommand(sublime_plugin.TextCommand):
+    content = ""
     def run(self, edit):
-        context = self.view.substr(sublime.Region(0, self.view.size()))
-
-        if (len(context) == 0):
-            return
-
-        address = self.view.file_name()
-        data = {"context" : context, "address" : address}
-        data = parse.urlencode(data).encode()
-        reg = request.Request("http://localhost:5342/", data=data)
-        page = urllib.request.urlopen(reg)
-        content = render_completions(page.read().decode())
-
-        if (len(content) == 0):
-            return
-
-        self.view.show_popup(content, flags=sublime.HTML, location=-1, max_width=400, on_navigate=self.on_choice_symbol)
+        for region in self.view.sel():
+            context = self.view.substr(sublime.Region(0, region.begin()))
+            if (len(context) == 0):
+                return
+            address = self.view.file_name()
+            data = {'context' : context, 'address' : address}
+            data = parse.urlencode(data).encode()
+            reg = request.Request("http://localhost:5342/", data=data)
+            page = urllib.request.urlopen(reg)
+            self.content = page.read().decode().split('\n')
+            if (len(self.content) == 0):
+                return
+            self.view.show_popup_menu(self.content, on_select=self.on_choice_symbol)
 
     def on_choice_symbol(self, symbol):
-        self.view.run_command("insert", {"characters": symbol})
+        self.view.run_command("insert", {"characters": self.content[symbol]})
         self.view.hide_popup()
